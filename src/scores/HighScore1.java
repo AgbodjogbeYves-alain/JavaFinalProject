5ffa7e85e1a6b6fe6c6b98d55ff4175e19b0af2f
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2016-05-03 22:26:57 +0200

    first commit'

---
+++ src/scores/HighScore1.java
@@ -0,0 +1,46 @@
+package scores;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * 
+ * @author Yves-alain Agbodjogbe and Nicolas Zambrano
+ *
+ */
+public class HighScore1 {
+	/**
+	 *
+	 * Function getScores()
+	 * @return Array of String : it's an array of scores from the csv on thinkspeak
+	 */
+	String[] getScores(){
+		try{
+		String url = "https://api.thingspeak.com/channels/112126/feeds.csv"; //url de notre channel
+
+		URL obj = new URL(url); //creation d'un objet URL
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection(); //ouverture de la connexion
+		int responseCode = con.getResponseCode(); //demande d'un get sur la connexion
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream())); //lecture du flux de la connexion
+		String inputLine=in.readLine(); //lecture du resultat
+		String[] scores=new String[100];
+		int n = 0;
+       while ((inputLine = in.readLine()) != null) {
+           scores[n]=inputLine;
+           n++;
+       }
+			//affectation du tableau de score dans un tableau avec la bonne longueur;
+   	String[] score=new String [n-1];
+     for(int i=0;i<n-1;i++){
+       	score[i]=scores[i];
+       }
+		return score;
+
+		}
+		catch(Exception e){
+			System.out.println(e);
+			 return null;
+		}
+	}
+}
