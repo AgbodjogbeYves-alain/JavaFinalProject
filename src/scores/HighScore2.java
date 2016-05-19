d249b95bd9ab0d003b8c1811828e27f153e1d009
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2016-05-19 10:31:48 +0200

    moi

--- src/scores/HighScore2.java
+++ src/scores/HighScore2.java
@@ -21,40 +21,46 @@ public class HighScore2 {
 	 */
 	public String[] getScores(){
 		try{
-		String url = "https://api.thingspeak.com/channels/112126/feeds.csv"; //url de notre channel
+			String url = "https://api.thingspeak.com/channels/112126/feeds.csv"; //url de notre channel
 
-		URL obj = new URL(url); //creation d'un objet URL
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection(); //ouverture de la connexion
-		int responseCode = con.getResponseCode(); //demande d'un get sur la connexion
-		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream())); //lecture du flux de la connexion
-		String inputLine=in.readLine(); //lecture du resultat
-		String[] scores=new String[100];
-		int n = 0;
-       while ((inputLine = in.readLine()) != null) {
-           scores[n]=inputLine;
-           n++;
-       }
+			URL obj = new URL(url); //creation d'un objet URL
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection(); //ouverture de la connexion
+			int responseCode = con.getResponseCode(); //demande d'un get sur la connexion
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream())); //lecture du flux de la connexion
+			String inputLine=in.readLine(); //lecture du resultat
+			String[] scores=new String[100];
+			int n = 0;
+			while ((inputLine = in.readLine()) != null) {
+				scores[n]=inputLine;
+				n++;
+			}
 			//affectation du tableau de score dans un tableau avec la bonne longueur;
-   	String[] score=new String [n-1];
-     for(int i=0;i<n-1;i++){
-       	score[i]=scores[i];
-       }
-		return score;
+			String[] score=new String [n-1];
+			for(int i=0;i<n-1;i++){
+				score[i]=scores[i];
+			}
+			return score;
 
 		}
 		catch(Exception e){
 			System.out.println(e);
-			 return null;
+			return null;
 		}
 	}
-	
-	
+
+
 	public BestPlayer[] tenBestScores(String[] readScores){
 		ArrayList<BestPlayer> allBest = new ArrayList<BestPlayer>();
 		String[] scores;
-		for (int i = 0; i < readScores.length; i++) {
+		for (int i = 0; i < readScores.length-1; i++) {
+			System.out.println(readScores[i]);
 			scores = readScores[i].split(",");
-			allBest.add(new BestPlayer(scores[3],Integer.parseInt(scores[1])));
+			int points = Integer.parseInt(scores[2]);
+			String name = scores[0];
+			if(name!=null){
+				allBest.add(new BestPlayer(name,points));
+			}
+
 		}
 		Collections.sort(allBest,Collections.reverseOrder());
 		BestPlayer[] top10 = new BestPlayer[10];
